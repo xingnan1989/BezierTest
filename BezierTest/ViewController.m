60c24e205ea8a885b558c2ef4cc811295300b1ee
xingnan1989 <179350761@qq.com>
2018-08-11 15:44:07 +0800

    first commit

--- BezierTest/ViewController.m
+++ BezierTest/ViewController.m
@@ -10,19 +10,30 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *test2ImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *TestImageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    //图片2圆形
+    self.test2ImageView.layer.cornerRadius =  50;
+    self.test2ImageView.layer.masksToBounds = YES;
+    //画贝塞尔曲线
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    //第一个圆弧
+    [path addArcWithCenter:CGPointMake(0, 50) radius:50 startAngle:M_PI/3 endAngle:M_PI*5/3 clockwise:NO];
+    //第二个圆弧
+    [path addArcWithCenter:CGPointMake(50, 50) radius:50 startAngle:M_PI*4/3 endAngle:M_PI*2/3 clockwise:YES];
+    //新layer
+    CAShapeLayer *shapeLayer = [[CAShapeLayer alloc] init];
+    shapeLayer.path = path.CGPath;
+    //用layer盖住图片
+    self.TestImageView.layer.mask = shapeLayer;
 }
 
 
